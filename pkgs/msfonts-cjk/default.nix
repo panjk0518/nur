25690aa45e1899e3e0f637d0fc1b9dc5a476d416
panjk0518 <panjk.panyx@gmail.com>
2025-04-21 22:45:34 +0800

    msfonts-cjk 1.000

--- pkgs/msfonts-cjk/default.nix
+++ pkgs/msfonts-cjk/default.nix
@@ -1,11 +1,14 @@
-{ stdenv, tar, zstd }:
+{ stdenv, tar, zstd, fetchUrl }:
 
 stdenv.mkDerivation {
   pname = "berkeley-mono-typeface";
   version = "1.000";
   description = "Microsoft CJK Fonts that I use and Segoe UI Fonts extracted from Windows 11";
 
-  src = ./assets/msfonts-cjk.tar.zst;
+  src = fetchUrl {
+    url = "https://raw.githubusercontents.com/panjk0518/nur/refs/heads/main/pkgs/msfonts-cjk/assets/msfonts-cjk.tar.zst";
+    hash = "sha512-8848c79895e103c2d5f0f4ffd17e8069e8f1c3d81c1ea454223f0921587d472d9ab6f0c13200ae688e974b97a49fc773596965cc950b55cf7aeedc8300a1c038";
+  };
 
   nativeBuildInputs = [
     tar
